69a481561b113f3c9f5eb56af4531551a735c18f
wutsi <herve.tchepannou@wutsi.com>
2022-06-03 20:45:21 -0400

    remove transaction approval

--- src/main/kotlin/com/wutsi/platform/qr/entity/EntityType.kt
+++ src/main/kotlin/com/wutsi/platform/qr/entity/EntityType.kt
@@ -5,6 +5,4 @@ enum class EntityType(val timeToLiveSeconds: Long) {
     ACCOUNT(100L * 365L * 84600L), // Static QR Code - 100 years
     PRODUCT(100L * 365L * 84600L), // Static QR Code - 100 years
     ORDER(100L * 365L * 84600L), // Static QR Code - 100 years
-    PAYMENT_REQUEST(300), // Dynamic QR Code - 5 minutes
-    TRANSACTION_APPROVAL(300), // Dynamic QR Code - 5 minutes
 }
